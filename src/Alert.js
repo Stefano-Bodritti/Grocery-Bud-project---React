093bd1cf5ca1ef1d70a66cbbdf949db62687c216
Stefano <tepo@hotmail.it>
2023-03-16 22:29:00 +0100

    show and hide alert

--- src/Alert.js
+++ src/Alert.js
@@ -1,7 +1,14 @@
 import React, { useEffect } from 'react'
 
-const Alert = () => {
-  return <h2>alert component</h2>
+const Alert = (props) => {
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      props.removeAlert();
+    }, 3000);
+    return () => clearTimeout(timeout);
+  }, []);
+
+  return <p className={`alert alert-${props.type}`}>{props.msg}</p>
 }
 
 export default Alert
--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
   const handleSubmit = (e) => {
     e.preventDefault();
     if (!name) {
-      // display alert
+      showAlert(true, "danger", "please enter value");
     } else if (name && isEditing) {
       // edit item
     } else {
@@ -24,10 +24,14 @@ function App() {
 
   }
 
+  const showAlert = (show=false, type='', msg='') => {
+    setAlert({show, msg, type});
+  }
+
   return (
     <section className="section-center">
       <form className="grocery-form" onSubmit={handleSubmit}>
-        {alert.show && <Alert />}
+        {alert.show && <Alert {...alert} removeAlert={showAlert} />}
         <h3>grocery bud</h3>
         <div className="form-control">
           <input
